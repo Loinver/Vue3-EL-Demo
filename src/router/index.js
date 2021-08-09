3056c15dd3e45307eefaf0fcdf1af0b2fc9c6fb0
Linyer <honeyliner@163.com>
2021-08-09 10:21:41 +0800

    update:修改个路由错误

--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,17 @@
  * @Author: Linyer
  * @Date: 2021-03-31 10:27:05
  * @LastEditors: Linyer
- * @LastEditTime: 2021-08-06 09:33:53
+ * @LastEditTime: 2021-08-09 10:21:13
  */
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
     path: '/',
-    redirect: '/index.html',
+    redirect: '/table',
   },
   {
-    path: '/table.html',
+    path: '/table',
     name: 'tableDemo',
     component: () => import(/* webpackChunkName: "table" */ '@/views/table/index.vue'),
     meta: {
@@ -27,7 +27,7 @@ const routes = [
     // 匹配所有路径  vue2使用*   vue3使用/:pathMatch(.*)*或/:pathMatch(.*)或/:catchAll(.*)
     path: '/:pathMatch(.*)*',
     name: '404',
-    redirect: '/index.html',
+    redirect: '/index',
   },
 ];
 const modulesFiles = require.context('./modules', true, /\.js$/);
