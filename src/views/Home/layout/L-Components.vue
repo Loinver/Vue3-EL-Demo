09d70ceab530bfc2c8eb2664d8571122added22b
Linyer <honeyliner@163.com>
2021-08-12 09:58:25 +0800

    feat(components): 新增拖拽组件，左侧组件列表先使用第三方配置

--- src/views/Home/layout/L-Components.vue
+++ src/views/Home/layout/L-Components.vue
@@ -4,7 +4,7 @@
  * @Author: Linyer
  * @Date: 2021-08-10 13:41:41
  * @LastEditors: Linyer
- * @LastEditTime: 2021-08-12 09:55:16
+ * @LastEditTime: 2021-08-12 09:57:10
 -->
 <template>
   <div class="components">
@@ -14,7 +14,6 @@
         class="components-draggable"
         :list="type.components"
         :group="{ name: 'componentsGroup', pull: 'clone', put: false }"
-        :clone="cloneComponent"
         item-key="name"
         draggable=".components-item"
         :sort="false"
@@ -22,7 +21,6 @@
         <template #item="{ element }">
           <div class="components-item">
             <div class="components-item-body">
-              <!-- <svg-icon :icon-class="element.__config__.tagIcon" /> -->
               {{ element.__config__.label }}
             </div>
           </div>
@@ -33,7 +31,6 @@
 </template>
 <script>
 import draggable from 'vuedraggable';
-import { deepClone } from '@/utils/index';
 import { defineComponent } from 'vue';
 import { inputComponents, selectComponents, layoutComponents } from '../components/config';
 export default defineComponent({
@@ -56,37 +53,8 @@ export default defineComponent({
         components: layoutComponents,
       },
     ];
-    let tempActiveData; // 当前模板数据
-    // 克隆组件
-    const cloneComponent = (origin) => {
-      const clone = deepClone(origin);
-      const config = clone.__config__;
-      config.span = this.formConf.span; // 生成代码时，会根据span做精简判断
-      createIdAndKey(clone);
-      clone.placeholder !== undefined && (clone.placeholder += config.label);
-      tempActiveData = clone;
-      return tempActiveData;
-    };
-    // 创建id和key
-    const createIdAndKey = (item) => {
-      const config = item.__config__;
-      config.formId = ++this.idGlobal;
-      config.renderKey = `${config.formId}${+new Date()}`; // 改变renderKey后可以实现强制更新组件
-      if (config.layout === 'colItem') {
-        item.__vModel__ = `field${this.idGlobal}`;
-      } else if (config.layout === 'rowItem') {
-        config.componentName = `row${this.idGlobal}`;
-        !Array.isArray(config.children) && (config.children = []);
-        delete config.label; // rowItem无需配置label属性
-      }
-      if (Array.isArray(config.children)) {
-        config.children = config.children.map((childItem) => this.createIdAndKey(childItem));
-      }
-      return item;
-    };
     return {
       allComponents,
-      cloneComponent,
     };
   },
 });
